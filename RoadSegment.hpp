5559f00ff3cb2fbe9a3f4216192e5e0e5ee70813
PaddaSecurity <50336501+PaddaSecurity@users.noreply.github.com>
2022-04-04 14:47:17 -0700

    Update RoadSegment.hpp

--- RoadSegment.hpp
+++ RoadSegment.hpp
@@ -1,12 +1,4 @@
-// RoadSegment.hpp
-//
-// ICS 46 Winter 2022
-// Project #5: Rock and Roll Stops the Traffic
-//
-// A RoadSegment structure describes the information we need to know
-// about a segment of road, namely the distance (in miles) that the
-// segment spans and the speed (in miles per hour) that traffic is
-// currently travelling on that road.
+
 
 #ifndef ROADSEGMENT_HPP
 #define ROADSEGMENT_HPP
