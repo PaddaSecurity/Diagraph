bc9ed824a4764d8a7dd03b713c31e7abdc606d9c
PaddaSecurity <50336501+PaddaSecurity@users.noreply.github.com>
2022-04-04 14:46:13 -0700

    Update RoadMap.hpp

--- RoadMap.hpp
+++ RoadMap.hpp
@@ -1,11 +1,3 @@
-// RoadMap.hpp
-//
-// ICS 46 Winter 2022
-// Project #5: Rock and Roll Stops the Traffic
-//
-// This header defines a type RoadMap, which is simply a shorthand name for a
-// particular instantiation of the Digraph template, where each vertex has a
-// string for its information and each edge has a RoadSegment for its information.
 
 #ifndef ROADMAP_HPP
 #define ROADMAP_HPP
